2f3eb4aea72c50c2b11c794a3326224dfe8dc16d
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-11-14 23:19:57 -0300

    Exercício

---
+++ mod12-Colecoes2/Exercicio_M12/src/colecoes/Parte1.java
@@ -0,0 +1,29 @@
+package colecoes;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Parte1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Digite os nomes separados por ',' ou 'Sair': ");
+            String nomes = input.nextLine();
+            if (nomes.equalsIgnoreCase("Sair")) {
+                break;
+            }
+
+            String[] arrayNomes = nomes.split(",");
+
+            System.out.println("Ordem Alfabética");
+            Arrays.sort(arrayNomes);
+
+            for (String nome : arrayNomes) {
+                System.out.println(nome.trim());
+            }
+        }
+        input.close();
+    }
+}
+
